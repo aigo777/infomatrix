edf842797a530c4c1642636aaf0eeb6d8fa6bb35
Aiganym <aiganym@Olzhass-MacBook-Pro.local>
2026-01-29 20:44:35 +0500

    Initial commit

---
+++ src/gaze_tracker.py
@@ -0,0 +1,17 @@
+class GazeTracker:
+    """
+    Handles webcam capture and MediaPipe Face Mesh processing.
+    Responsible ONLY for raw gaze extraction.
+    """
+
+    def __init__(self):
+        pass
+
+    def get_iris_position(self):
+        pass
+
+    def is_face_detected(self):
+        pass
+
+    def get_raw_landmarks(self):
+        pass
